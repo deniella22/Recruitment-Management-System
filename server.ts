25089312c690da98ae29158acb15061ec1f48653
Deniella Halili <halili.deniella@biga.onesms.edu.ph>
2026-08-14 16:53:23 +0800

    Update server.ts

--- server.ts
+++ server.ts
@@ -8,8 +8,7 @@ import { User, StudentRecord, AdmissionStatus, SystemSettings } from './src/type
 
 async function startServer() {
   const app = express();
-  const PORT = 3000;
-
+  const PORT = Number(process.env.PORT) || 3000;
   app.use(express.json({ limit: '50mb' }));
   app.use(express.urlencoded({ limit: '50mb', extended: true }));
 
